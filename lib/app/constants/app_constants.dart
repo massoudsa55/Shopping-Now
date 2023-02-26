eddf20faee8857c85c14bebe10d2c9b9574e4356
Messaoud_Ben <33179371+massoudsa55@users.noreply.github.com>
2023-02-26 21:41:30 +0100

    add mapper for convert ModelResponse to Model

--- lib/app/constants/app_constants.dart
+++ lib/app/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const int splashScreenDelay = 3;
+  static const int splashScreenDelay = 4;
   static const String baseUrl = "https://shopping.mocklab.io/";
   static const String empty = "";
   static const int zero = 0;
